3e6fa804547fc6ee26efd070a7a62b28b5accc0b
Caio Figueiredo <Contascontao@gmail.com>
2024-02-11 18:33:33 -0300

    lop

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from TikTokApi import TikTokApi
 from flask import Flask, request, jsonify, render_template
-from playwright.sync_api import BrowserContext, Browser
-import os
+from googleapiclient.discovery import build
+import firebase_admin
+from firebase_admin import credentials, db, firestore
 import asyncio
 
 
@@ -14,20 +15,54 @@ context = {
 }
 
 
-async def getVideos():
+async def getTiktokVideos():
     async with TikTokApi() as api:
 
         await api.create_sessions(ms_tokens=[ms_token], num_sessions=1, sleep_after=5, headless=True, context_options=context)
-        videos = [video async for video in api.user(username="aaaldv").videos()]
-        videos.sort(key=lambda x: x['createTime'], reverse=True)
-        newest_video = videos[0] if videos else None
-        return newest_video
+        videos = []
+        async for video in api.user(username="aaaldv").videos():
+            videos.append({"create_time": video.create_time, "id": video.id})
+            videos.sort(key=lambda x: x['create_time'], reverse=True)
+            return videos[0]['id']
+
+
+def GetGoogleSheets():
+
+    # Fetch the service account key JSON file contents
+    cred = credentials.Certificate('sheetviewerkey.json')
+
+    # Initialize the app with a service account, granting admin privileges
+
+    if firebase_admin._apps.__len__() == 0:
+        firebase_admin.initialize_app(cred)
+    db = firestore.client()
+    prods = []
+    for document in db.collection(u'prods').stream():
+        prods.append({
+            "name": document.to_dict()["name"],
+            "price": document.to_dict()["price"],
+            "details": document.to_dict()["details"],
+            "imglink": "static/img/"+document.to_dict()["imglink"]
+        })
+
+    events = []
+    for document in db.collection(u'events').stream():
+        events.append({
+            "name": document.to_dict()["name"],
+            "date": document.to_dict()["date"],
+            "details": document.to_dict()["details"],
+            "imglink": "static/img/"+document.to_dict()["imglink"],
+            "dtlink": document.to_dict()["dtlink"]
+        })
+    return events, prods
 
 
 @app.route('/')
 def about():
-    newest_video = asyncio.run(getVideos())
-    return render_template("html/index.html")
+    tiktokid = asyncio.run(getTiktokVideos())
+    tiktokvideo = "https://www.tiktok.com/@aaaldv.einstein/video/" + tiktokid
+    events, products = GetGoogleSheets()
+    return render_template("html/index.html", events=events, products=products, tiktokvideo=tiktokvideo, tiktokid=tiktokid)
 
 
 if __name__ == '__main__':
