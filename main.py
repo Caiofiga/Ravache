6a465f935a82405534a06107a223e2dd8d7b90dd
Caio Figueiredo <Contascontao@gmail.com>
2024-02-14 15:55:47 -0300

    added prices, and a lot more stuff I dont really remember. ;)

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def GetGoogleSheets():
         doc_dict = document.to_dict()  # Fetch document data once to optimize
         prods.append({
             "name": doc_dict["name"],
-            "price": doc_dict["price"],
+            "price": f"R${doc_dict['price']}",
             "details": doc_dict["details"],
             "imglink": f"static/img/{doc_dict['imglink']}",
             "id": document.id
@@ -57,20 +57,22 @@ def GetGoogleSheets():
             days = delta.days % 30  # Calculate the remainder of days
 
             # Event is in the future and closer than any previously found
-            event['date'] = f"{months} {'month' if months >= 1 else 'months'}, {days} {'day' if days >= 1 else 'days'} from now"
+            event['date'] = f"{months} {'month' if months <= 1 else 'months'}, {days} {'day' if days <= 1 else 'days'} from now"
 
-        if timedelta(0) <= delta < closest_delta:
-            if closest_event_index is not None:
-                events[closest_event_index]["isMain"] = False
+    # Check if this event is the closest future event so far
+            if delta < closest_delta:
+                # Mark previous closest event as not main if it exists
+                if closest_event_index is not None:
+                    events[closest_event_index]["isMain"] = False
+
                 closest_delta = delta
                 closest_event_index = index
                 event["isMain"] = True
 
-    # Ensure only the closest future event is marked as main
+    # Ensure only the closest event is marked as main after the loop
     if closest_event_index is not None:
-        for i, event in enumerate(events):
-            if i != closest_event_index:
-                event["isMain"] = False
+        for index, event in enumerate(events):
+            event["isMain"] = index == closest_event_index
 
     return events, prods
 
@@ -83,7 +85,8 @@ def about():
 
 @app.route('/admin')
 def admin():
-    return render_template("admin.html")
+    events, products = GetGoogleSheets()
+    return render_template("admin.html", events=events, products=products)
 
 
 if __name__ == '__main__':
